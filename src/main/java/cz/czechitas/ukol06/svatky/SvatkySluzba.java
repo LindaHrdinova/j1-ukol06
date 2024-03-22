679c331966746f11a578a91f21e9b63fab4ed162
Linda Hrdinová <hrlin@seznam.cz>
2024-03-22 15:57:13 +0100

    homework 'SvatkySluzby'

--- src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
+++ src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.time.MonthDay;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class SvatkySluzba {
 
@@ -29,13 +30,18 @@ public class SvatkySluzba {
 
     public List<String> vyhledatSvatkyKeDni(MonthDay day) {
         // TODO
-        // získat seznam svátků
-        // převést na Stream
-        // pomocí metody filter() vybrat jen ty, které odpovídají zadanému dni (porovnat MonthDay pomocí metodyequals())
-        // pomocí metody map() získat z objektu jméno
-        // pomocí toList() převést na List
+        seznamSvatku.setSvatky(seznamSvatku.getSvatky());
 
-        // Následující řádek po vlastní implementaci smažete.
-        return List.of();
+        return Stream.of(seznamSvatku)
+                .filter(Svatek -> Svatek.getSvatky().getFirst().getDen().equals(day))
+                .map(Svatek -> seznamSvatku.getSvatky().getFirst().getJmeno())
+                .toList();
+
+         /* Stream<SeznamSvatku> seznamSvatkuStream = (Stream<SeznamSvatku>) seznamSvatku;
+        return seznamSvatkuStream
+                .filter(Svatek -> Svatek.getSvatky().getFirst().getDen().equals(day))
+                .map(Svatek -> seznamSvatku.getSvatky().getFirst().getJmeno())
+                .toList();*/
+
     }
 }
