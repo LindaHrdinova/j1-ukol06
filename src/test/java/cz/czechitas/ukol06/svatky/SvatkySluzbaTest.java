0b61e4da506732f5cde5dc0e5ad4b178cf3f3f20
Linda Hrdinová <hrlin@seznam.cz>
2024-03-25 00:04:00 +0100

    homework - corrections 0.3

--- src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
+++ src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
@@ -2,17 +2,20 @@ package cz.czechitas.ukol06.svatky;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.time.Month;
 import java.time.MonthDay;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SvatkySluzbaTest {
     @Test
-    void vyhledatSvatkyKeDni() {
+    void vyhledatSvatkyKeDni() throws IOException {
         //TODO implementovat test metody vyhledatSvatkyKeDni
-        //String denTest = "--03-18";
-        String jmenoTest = "Eduard";
-        String vysledek = vyhledatSvatkyKeDni(MonthDay.of(3, 18));
-        assertEquals(jmenoTest, vysledek);
+        String denTest = "--03-18";
+        List<String> jmenoTest = List.of("Eduard");
+        SvatkySluzba svatky = new SvatkySluzba();
+        assertEquals(jmenoTest, svatky.vyhledatSvatkyKeDni(MonthDay.of(3,18)));
     }
 }
