b660b83c9931638cf56c96967514f9f9fbfe9f75
Linda Hrdinová <hrlin@seznam.cz>
2024-03-26 14:46:11 +0100

    homework feedback 0.1

--- src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
+++ src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
@@ -10,35 +10,26 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SvatkySluzbaTest {
+
+    SvatkySluzba svatky = new SvatkySluzba();
+
+    SvatkySluzbaTest() throws IOException {
+    }
+
     @Test
-    void vyhledatSvatkyKeDni() throws IOException {
-        //TODO implementovat test metody vyhledatSvatkyKeDni
-        String denTest = "--03-18";
+    void vyhledatSvatkyKeDni() {
         List<String> jmenoTest = List.of("Eduard");
-        SvatkySluzba svatky = new SvatkySluzba();
         assertEquals(jmenoTest, svatky.vyhledatSvatkyKeDni(MonthDay.of(3, 18)));
     }
 
     @Test
-    void vyhledatVicSvatkuKeDni() throws IOException {
-        //String denTest = "--09-01";
+    void vyhledatVicSvatkuKeDni() {
         List<String> jmenaTest = List.of("Linda", "Samuel", "Jiljí");
-        SvatkySluzba svatky = new SvatkySluzba();
         assertEquals(jmenaTest, svatky.vyhledatSvatkyKeDni(MonthDay.of(9, 1)));
     }
 
     @Test
-    void vyhledatNesvatekKeDni() throws IOException {
-        //String denTest = "--01-01";
-        List<String> nejmenaTest = List.of("");
-        SvatkySluzba svatky = new SvatkySluzba();
-        assertEquals(nejmenaTest, svatky.vyhledatSvatkyKeDni(MonthDay.of(1, 1)));
-    }
-
-    @Test
-    void vyhledatNesvatekKeDni2() throws IOException {
-        //String denTest = "--01-01";
-        SvatkySluzba svatky = new SvatkySluzba();
-        assertEquals(true, svatky.vyhledatSvatkyKeDni(MonthDay.of(1, 1)).isEmpty());
+    void vyhledatNesvatekKeDni() {
+        assertTrue(svatky.vyhledatSvatkyKeDni(MonthDay.of(1, 1)).isEmpty());
     }
 }
